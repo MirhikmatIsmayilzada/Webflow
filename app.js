8dfb3f1c0e0424c805e9882da7c907d61091aade
MirhikmatIsmayilzada <mirhikmatismayilzada@gmail.com>
2023-05-30 15:30:58 +0000

    end

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ $('.add').click(function (e) {
     e.preventDefault();
     if (!$.trim($('input').val()) == '') {
         let a = b => {
-            for (let i = 0; i < b; i++) ($('.button').append(`<h3>${b}</h3>`))
+            for (let i = 0; i < b; i++) ($('.minibox').append(`<h3>${b}</h3>`))
         }
-        let c = d => Number(d) ? a(d) : $('.button').append(`<h3>${d}</h3>`)
+        let c = d => Number(d) ? a(d) : $('.minibox').append(`<h3>${d}</h3>`)
         c($('input').val())
     }
     $('input').val('');
